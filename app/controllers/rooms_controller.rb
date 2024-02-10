52d0f163663a6abef4a0f6fe3dd3d2d6e6429a05
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-02-10 21:14:38 +0300

    created room controller endpoints

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,19 @@
 class RoomsController < ApplicationController
+    def index 
+        @room = Room.all
+    end
+
+    def create
+        @room = Room.create(room_params)
+        if @room
+            render json: @room, status: :created
+        else
+            render json: @room.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def room_params
+        Room.params(:room_number, :hotel_id)
+    end
 end
