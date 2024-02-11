51e32e9eb10099118d9a51dea5ba93a2978fe545
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-02-11 16:06:02 +0300

    changes: footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,12 +4,15 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <footer className="flex items-center fixed bottom-0 mb-2 p-6">
-        <div className='container '>
-            <ul>
-                <Link to={'/'}>Home</Link>
-                <Link to={'/about'}>About</Link>
-                <Link to={'/bookings'}>Bokokings</Link>
-            </ul>
+        <div className='links-left flex flex-col justify-start gap-3'>
+            <Link to={'/'}>Home</Link>
+            <Link to={'/about'}>About</Link>
+            <Link to={'/bookings'}>Bookings</Link>
+        </div>
+        <div className='links-right flex flex-col justify-end float-right gap-3'>
+            <Link to={'/'}>Home</Link>
+            <Link to={'/about'}>About</Link>
+            <Link to={'/bookings'}>Bookings</Link>
         </div>
     </footer>
   )
