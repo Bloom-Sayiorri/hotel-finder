15ba5a152ff8f0475645da3223307c3bcab77053
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-02-14 23:49:31 +0300

    create: user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+  def index
+    @user = User.all
+  end
+
+  def show
+    @user = find_user
+    if @user.present?
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :not_found
+    end
+  end
+
+  
 end
