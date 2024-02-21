c806bd8ccfbe0447415d774bf2e8f0e362658a77
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-02-21 21:57:31 +0300

    edited: about.jsx layout

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,9 +3,20 @@ import about_img from '../assets/about_img.jpg'
 
 const About = () => {
   return (
-    <div className="">
-      <p></p>
-          <img className="" src={about_img} alt="About"/>
+    <div className="flex flex-row justify-center items-center m-8">
+      <section className="flex flex-col justify-center items-center text-lg leading-loose overflow-normal">
+        {/* mb-8 not working in parent section*/}
+        <p className='mb-2'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate blanditiis magni similique molestiae, quam quia earum libero dicta unde voluptates ex sit ipsam. Fugit quaerat porro enim tempore? Nesciunt, quam.
+        </p>
+        <p className='mb-2'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente natus molestiae incidunt accusamus ratione, maxime culpa tempora distinctio cum eligendi veniam libero, corporis fugiat eaque ipsam cumque quaerat fugit nesciunt?
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente natus molestiae incidunt accusamus ratione, maxime culpa tempora distinctio cum eligendi veniam libero, corporis fugiat eaque ipsam cumque quaerat fugit nesciunt?
+        </p>
+      </section>
+      <img className="h-full w-1/2 rounded" src={about_img}alt="About"/>
     </div>
   )
 }
