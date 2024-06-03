219d188fd0a1a76e1cbecef594eba193901fe2de
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-06-03 23:25:35 +0300

    created client routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+// import { useEffect } from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Listings from './pages/Listings';
 
 function App() {
+  // const [ hotels, setHotels ] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("/api/")
+  //   .then((r) => r.json())
+  //   .then((data) => console.log(data))
+  // }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Home />}/>
+        <Route exact path='/about' element={<About />}/>
+        <Route exact path='/listings' element={<Listings />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
