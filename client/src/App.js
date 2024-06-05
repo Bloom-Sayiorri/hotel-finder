ca6a923e9985c305d8f93261d84ff9971dce27ee
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-06-05 18:26:57 +0300

    Navbar styling and strucutre done

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 // import { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Listings from './pages/Listings';
@@ -16,6 +17,7 @@ function App() {
 
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route exact path='/' element={<Home />}/>
         <Route exact path='/about' element={<About />}/>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../styles/navbar.css'
+import { NavLink } from 'react-router-dom'
+import logo from '../assets/hotel.svg'
+import { IoMenu } from "react-icons/io5";
+import { RxAvatar } from "react-icons/rx";
 
 const Navbar = () => {
+  const [ isOpen, setIsOpen ] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <header className="header">
+      <div className='logo'>
+        <img src={logo} alt="Logo" />
+        <p className="logo-header">Hotel Findr</p>
+      </div>
+      <nav className="nav">
+        <NavLink to='/' activeClassName="active" className="nav-links hover">Home</NavLink>
+        <NavLink to='/about' className="nav-links">About</NavLink>
+        <NavLink to='/listings' className="nav-links">Listings</NavLink>
+      </nav>
+      <div className="toggle-btns">
+        <IoMenu className="menu-btn" onClick={() => {setIsOpen(isOpen => !isOpen); console.log(isOpen);}}/>
+        <RxAvatar className="avatar-btn" onClick={() => {}}/>
+      </div>
+    </header>
+    
   )
 }
 
