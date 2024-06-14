657961ad14351c0b95d80d1b00692770d09e4bf3
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-06-14 22:07:38 +0300

    footer page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import '../styles/footer.css'
+import { FaXTwitter, FaInstagram } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className='container'>
+        <p className='text'>All rights reserved.</p>
+        <p className='text'><span style={{color: "blue"}}>&copy;</span> Hotel Findr</p>
+      </div>
+      
+      <div className='rule'></div>
+      <div className='socials'>
+        <FaXTwitter className='twitter'/>
+        <FaInstagram className='instagram'/>
+      </div>
+    </footer>
   )
 }
 
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -10,7 +10,10 @@ const About = () => {
       </section>
       <section className='text-section'>
         <p>
-          Welcome to Hotel Findr, where innovation meets excellence. Since our founding, we have been committed to providing top-notch services and solutions to our clients. Our dedicated team of professionals brings together expertise, creativity, and a passion for delivering results that exceed expectations.
+          Welcome to Hotel Findr, where innovation meets excellence. Since our founding, we have been committed to providing top-notch services and solutions to our clients.
+        </p>
+        <p>
+          Our dedicated team of professionals brings together expertise, creativity, and a passion for delivering results that exceed expectations.
         </p>
         <p>
           Thank you for choosing Hotel Findr. We look forward to partnering with you on your journey to success. Together, we can achieve great things.
