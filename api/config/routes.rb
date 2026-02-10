40803eb7a27a82f553a582f2ca83e99f67ef627f
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2026-02-10 10:52:06 +0300

    config, schema and gem dependnecy changes

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
-  resources :rooms
-  resources :hotels
+  resources :messages
+  resources :favorites
+  resources :reviews
+  resources :bookings
+  resources :hosts
   resources :listings
   resources :guests
+  resources :users, only: %i[index create destroy show]
+
+  get 'users/me', to: 'users#me'
+  post 'login', to: 'auth#create'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
